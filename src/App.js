3e3b130c073352f6337da582768b4bef6d06cce9
Nithin <nithin.code1@gmail.com>
2020-08-01 12:41:08 +0530

    deploying first react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-primary py-5">
+      <h1 className="text-light">React App Deployment Test</h1>
+      <hr />
+      <h3 className="text-light">Author: Nithin Nair</h3>
+      <hr />
+      <img
+        className="border-radius-20px"
+        src="https://source.unsplash.com/600x400/?coding,html"
+      />
+      <br />
+
+      <br />
+      <p className="text-light">
+        This is just a react deployment test and hence doesn't have anything
+        much to offer
+      </p>
     </div>
   );
 }
