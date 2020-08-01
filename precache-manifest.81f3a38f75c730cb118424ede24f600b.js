67bed2e910f6b57a2ff3808e894600151fb6013d
Nithin <nithin.code1@gmail.com>
2020-08-01 12:42:22 +0530

    Updates

---
+++ precache-manifest.81f3a38f75c730cb118424ede24f600b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15213f5d7409d44c478088f69514b0ac",
+    "url": "/react-deploy-test/index.html"
+  },
+  {
+    "revision": "45266b98735a0b33e799",
+    "url": "/react-deploy-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0d4198e13dad39fb5bd4",
+    "url": "/react-deploy-test/static/js/2.4de9402e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy-test/static/js/2.4de9402e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45266b98735a0b33e799",
+    "url": "/react-deploy-test/static/js/main.a1dc2dbf.chunk.js"
+  },
+  {
+    "revision": "d3e2f413c23f27275079",
+    "url": "/react-deploy-test/static/js/runtime-main.4964af8a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
